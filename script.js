a98f25486e2e8aabbb6d8d6a9a8c83d3799f98a1
Viitorsoares <viitorgab.soares@gmail.com>
2025-07-29 15:47:56 -0300

    Finalizando o header

---
+++ script.js
@@ -0,0 +1,11 @@
+const header = document.getElementById('header');
+
+if (header instanceof HTMLElement) {
+  window.addEventListener('scroll', () => {
+    if (window.scrollY >= 100) {
+      header.style.background = "#191919";
+    } else {
+      header.style.background = 'transparent';
+    }
+  });
+}
