8a48fead37fa8af61425daf235cc0afbea6ea15e
Viitorsoares <viitorgab.soares@gmail.com>
2026-09-03 21:42:23 -0300

    Adicionando header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,14 @@
+import { Code } from 'lucide-react';
+
 export default function Header() {
     return (
-        <div>
-            
-        </div>
+        <header className='w-full h-screen'>
+            <nav className='px-4'>
+                <div className='flex gap-2 text-xl'>
+                    <Code className='text-main-green w-6 h-6' />
+                    <h1 className='text-main-text'>Vitor</h1>
+                </div>
+            </nav>
+        </header>
     )
 }
