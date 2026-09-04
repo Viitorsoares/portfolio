d0e5165e137bf3cbc9f38e143507c1e4ba456596
Viitorsoares <viitorgab.soares@gmail.com>
2026-09-04 17:02:10 -0300

    Adicionado o footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,7 +1,23 @@
 export default function Footer() {
     return (
-        <div>
-            
-        </div>
+        <footer className="p-5 text-left text-sm">
+            <div className="text-secondary-text">
+                <div className="flex flex-row gap-4">
+                    &copy;
+                    <p>2026</p>
+                    <p>Vitor Soares</p>
+                </div>
+
+                <div>
+                    <p className="flex flex-row gap-1.5">
+                        Construido com
+                        <span>Next.js</span>
+                        <span>TypeScript</span>
+                        <span>Tailwind CSS</span>
+                        <span>Shadcn/ui</span>
+                    </p>
+                </div>
+            </div>
+        </footer>
     )
 }
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,7 +3,7 @@ import { Code, Menu } from 'lucide-react';
 export default function Header() {
     return (
         <header className='w-full h-screen'>
-            <nav className='flex justify-between px-6 mt-4'>
+            <nav className='flex justify-between px-5 mt-4'>
                 <div className='flex gap-2 text-xl'>
                     <Code className='text-main-green w-6 h-6' />
                     <h1 className='text-main-text'>Vitor</h1>
