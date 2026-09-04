3d7981b5af69caae615e0293a4bea99dba3d176d
Viitorsoares <viitorgab.soares@gmail.com>
2026-09-04 16:11:12 -0300

    Criado o header estatuci

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,12 +1,19 @@
-import { Code } from 'lucide-react';
+import { Code, Menu } from 'lucide-react';
 
 export default function Header() {
     return (
         <header className='w-full h-screen'>
-            <nav className='px-4'>
+            <nav className='flex justify-between px-6 mt-4'>
                 <div className='flex gap-2 text-xl'>
                     <Code className='text-main-green w-6 h-6' />
                     <h1 className='text-main-text'>Vitor</h1>
+                </div>
+
+                <div>
+                    <div>
+                        
+                    </div>
+                    <Menu className='text-main-text' />
                 </div>
             </nav>
         </header>
